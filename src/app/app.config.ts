7b812ad129cf8955f13a6c79f7a241518389726c
giseleMM <giselemedina01@gmail.com>
2024-05-02 00:33:50 -0300

    agregando environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,10 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"sjuegos-dd88f","appId":"1:834618985942:web:f3bec234b35414f51c6877","storageBucket":"sjuegos-dd88f.appspot.com","apiKey":"AIzaSyCUjGQoPdQTRNdFnfQnEzJuNE8Aqx6l244","authDomain":"sjuegos-dd88f.firebaseapp.com","messagingSenderId":"834618985942"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers:
+   [provideRouter(routes), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    firebaseConfig: {"projectId": "sjuegos-dd88f", "appId": "1:834618985942:web:f3bec234b35414f51c6877", "storageBucket": "sjuegos-dd88f.appspot.com", "apiKey": "AIzaSyCUjGQoPdQTRNdFnfQnEzJuNE8Aqx6l244", "authDomain": "sjuegos-dd88f.firebaseapp.com", "messagingSenderId": "834618985942" }
+
+}
+
